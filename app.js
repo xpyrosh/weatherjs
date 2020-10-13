08143be709c9f1a1cf98e7b85182c99ef448fe78
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2020-10-12 22:35:42 -0400

    included jquery so modal save works, made change location button active

--- app.js
+++ app.js
@@ -7,6 +7,21 @@ const ui = new UI();
 // Event Listener for DOM Load
 document.addEventListener('DOMContentLoaded', getWeather);
 
+// Change Location Event
+document.getElementById('w-change-btn').addEventListener('click', (e)=> {
+    const city = document.getElementById('city').value;
+    const state = document.getElementById('state').value;
+
+    // Change the location
+    weather.changeLocation(city, state);
+
+    // Get Weather Again
+    getWeather();
+
+    // Close Modal - NEEDS JQUERY
+    $('#locModal').modal('hide');
+});
+
 
 // Get Weather
 function getWeather(){
